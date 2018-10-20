818730da891c89036c5372bad9672717fae33787
Roxane Letourneau <roxaneletourneau2@gmail.com>
2018-10-20 09:02:28 -0400

    test

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { DataModule } from './data/data.module';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { ReportFireComponent } from './report-fire/report-fire.component';
+import { RouterModule } from '@angular/router',
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    ReportFireComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      { path: '', component: ReportFireComponent}
+    ]),
     BrowserModule,
     DataModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
---
+++ frontend/src/app/report-fire/report-fire.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-fire',
+  templateUrl: './report-fire.component.html',
+  styleUrls: ['./report-fire.component.css']
+})
+export class ReportFireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
